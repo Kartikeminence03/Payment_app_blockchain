30141448b6dcaea172f76171250b79cb840c351b
Kartikemenence03 <kartik.eminence@gmail.com>
2023-11-16 18:17:02 +0530

    Update

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -18,12 +18,13 @@ const Cart = () => {
   const [payInput, setPayInput] = useState("");
   const [tokenETH,setTokenETH] = useState();
   const [buttonDisabled, setButtonDisabled] = useState(false);
-  const [account, setAccount] = useState()
+  const [account, setAccount] = useState("");
+  const [metaAccount, setMetaAccount] = useState()
 
   const loadBlockchainData = async()=>{
     const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
     const account = ethers.utils.getAddress(accounts[0])
-    setAccount(account)
+    // setMetaAccount(account)
   }
 
   useEffect(()=>{
@@ -39,6 +40,10 @@ const Cart = () => {
     setTokenETH(payinput_inNum/8)
   };
 
+  const account_id = (event)=>{
+    setAccount(event.target.value)
+  };
+
   const eth = [{tokenPrice:payinput_inNum, toETH:tokenETH, userAccount:account, quant:1}]
 
   const makePayment = async ()=>{
@@ -51,6 +56,12 @@ const Cart = () => {
   const result = stripe.redirectToCheckout({
       sessionId:session.id
   });
+
+  console.log(await result);
+
+  // if(!result===true){
+  //   paymentDB()
+  // }
   
   if(result.error){
       console.log(result.error);
@@ -88,20 +99,34 @@ const Cart = () => {
 
             <div className='receive-container'>
               <span className='receive-label'>Receive: </span>
-              <div class="progress-input receive-input" style={style1}>ETH: {!payInput?0:tokenETH}</div>
-              {/* <span class="gary-span backcolor">GARYS</span> */}
+              <div className="progress-input receive-input" style={style1}>ETH: {!payInput?0:tokenETH}</div>
+              {/* <span className="gary-span backcolor">GARYS</span> */}
             </div>
 
 
-            <button class="claim-button" scale="md" id="claim" disabled="" 
+            <button className="claim-button" scale="md" id="claim" disabled="" 
             style={{"margin-top": "40px;"}}>PAY</button>
             {/* <a href='https://global-stg.transak.com/?apikey=8f020938-fd46-4977-bc01-059542dc79b7' 
             // target="_blank"
-            rel="noreferrer" class="claim-button" scale="md" id="claim" disabled="" 
+            rel="noreferrer" className="claim-button" scale="md" id="claim" disabled="" 
             style={{"margin-top": "40px;"}}>Buy To Card</a> */}
 
-            {payInput == ""?<button class="claim-button" scale="md" id="claim" disabled={!buttonDisabled} 
-            style={{"margin-top": "40px;"}} onClick={()=>makePayment()}>Buy With Card ST</button>:<button class="claim-button" scale="md" id="claim" disabled={buttonDisabled} 
+           <div className='pay-container'>
+              <span className='pay-label1'>You account ID</span>
+              <div style={style}>
+                <input 
+                scale="md" 
+                className="progress-input pay-input1" 
+                id="pay-input1" 
+                onChange={account_id}
+                type='text'
+                value={account} 
+                style={{"visibility":"hidden;"}}/>
+              </div>
+            </div>
+
+            {payInput && account == ""?<button className="claim-button" scale="md" id="claim" disabled={!buttonDisabled} 
+            style={{"margin-top": "40px;"}} onClick={()=>makePayment()}>Buy With Card ST</button>:<button className="claim-button" scale="md" id="claim" disabled={buttonDisabled} 
             style={{"margin-top": "40px;"}} onClick={()=>makePayment()}>Buy With Card ST</button>}
         </div>
     </div>
